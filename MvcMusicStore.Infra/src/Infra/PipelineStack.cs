5c875ec83f9a73fd341eaa252f79306dcd354f43
Ulili Nhaga <3706665+ulili5@users.noreply.github.com>
2021-11-03 11:16:04 -0700

    add CodePipeline

---
+++ MvcMusicStore.Infra/src/Infra/PipelineStack.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Amazon.CDK;
+using Amazon.CDK.AWS.CodeCommit;
+using Amazon.CDK.AWS.CodePipeline;
+using Amazon.CDK.AWS.CodePipeline.Actions;
+using Amazon.CDK.Pipelines;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.Ecr.Assets;
+using Amazon.CDK.AWS.ECS;
+using Amazon.CDK.AWS.ECS.Patterns;
+
+namespace Infra
+{
+    public class PipelineStack : Stack
+    {
+        public PipelineStack(Construct parent, string id, IStackProps props = null) : base(parent, id, props)
+        {
+            var repo = new Repository(this, "WorkshopRepo", new RepositoryProps
+            {
+                RepositoryName = "MusicStoreRepo"
+            });
+
+            var pipeline = new CodePipeline(this, "pipeline", new CodePipelineProps
+            {
+                PipelineName = "MyPipeline",
+                Synth = new ShellStep("Synth", new ShellStepProps
+                {
+                    Input = CodePipelineSource.CodeCommit(repo, "SourceCode") ,
+                    Commands = new string[] { 
+                            "npm install -g aws-cdk", 
+                            "cd MvcMusicStore.Infra",
+                            "cdk synth" 
+                        }
+                })
+            });
+        }
+
+    }
+}
