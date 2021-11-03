7777cff80495b964782f1a141b6bf411155ecfcb
Ulili Nhaga <3706665+ulili5@users.noreply.github.com>
2021-11-03 11:15:20 -0700

    add CodePipeline

--- MvcMusicStore.Infra/src/Infra/Program.cs
+++ MvcMusicStore.Infra/src/Infra/Program.cs
@@ -10,35 +10,35 @@ namespace Infra
         public static void Main(string[] args)
         {
             var app = new App();
-            new InfraStack(app, "WebAppInfraStack", new StackProps
-            {
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+            // new InfraStack(app, "WebAppInfraStack", new StackProps
+            // {
+            //     // If you don't specify 'env', this stack will be environment-agnostic.
+            //     // Account/Region-dependent features and context lookups will not work,
+            //     // but a single synthesized template can be deployed anywhere.
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /* */
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-                    Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
-                }
+            //     // Uncomment the next block to specialize this stack for the AWS Account
+            //     // and Region that are implied by the current CLI configuration.
+            //     /* */
+            //     Env = new Amazon.CDK.Environment
+            //     {
+            //         Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+            //         Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
+            //     }
                 
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = "111122223333",
-                    Region = "us-west-2",
-                }
-                */
-                
+            //     // Uncomment the next block if you know exactly what Account and Region you
+            //     // want to deploy the stack to.
+            //     /*
+            //     Env = new Amazon.CDK.Environment
+            //     {
+            //         Account = "111122223333",
+            //         Region = "us-west-2",
+            //     }
+            //     */
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-            });
+            //     // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+            // });
+            new PipelineStack(app, "PipelineStack");
 
             app.Synth();
         }
