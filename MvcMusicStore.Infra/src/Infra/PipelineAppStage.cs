5b04e9dd8116271abc46d73aafa3265d9845c833
Ulili Nhaga <3706665+ulili5@users.noreply.github.com>
2021-11-03 13:54:55 -0700

    rename the infraStackName

--- MvcMusicStore.Infra/src/Infra/PipelineAppStage.cs
+++ MvcMusicStore.Infra/src/Infra/PipelineAppStage.cs
@@ -6,7 +6,7 @@ namespace Infra
     {
         public PipelineAppStage(Construct scope, string id, StageProps props=null) : base(scope, id, props)
         {
-            Stack infraStack = new InfraStack(this, "AppInfraStack", new StackProps{
+            Stack infraStack = new InfraStack(this, "AppStack", new StackProps{
                 Env = new Environment{
                     Account = props.Env.Account,
                     Region = props.Env.Region
